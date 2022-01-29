15591dd4260c981d2bedf3469274c5838e9b7b2d
abhisheksingh6167 <94945890+abhisheksingh6167@users.noreply.github.com>
2022-01-29 15:33:52 -0500

    Create expediahw.java

---
+++ expediahw.java
@@ -0,0 +1,49 @@
+package PeopleNTech.Automationtraining;
+
+import org.testng.annotations.Test;
+
+import Common.common3;
+import PageObjectModel.expedi;
+
+public class expediaHw extends common3{
+  @Test
+  public void f() {
+	   
+	  expedi obj1 = new expedi(driver);
+	  
+	  obj1.fromm();
+	  
+	  obj1.too();
+	  
+	  obj1.searcho();
+	  
+	  obj1.roundo();
+	  
+	  obj1.roundt();
+	  
+	  obj1.flo();
+	  
+	  obj1.flt();
+	  
+	  obj1.flthree();
+	  
+	  obj1.flfour();
+	  
+	  obj1.flfive();
+	  
+	  obj1.firstname();
+	  
+	  obj1.lastname();
+	  
+	  obj1.phoneNumber();
+	  
+	  obj1.genderMale();
+	  
+	  obj1.bdayMonth();
+	  
+	  obj1.bdayDay();
+	  
+	  obj1.bdayYear();   
+	  
+  }
+}
